5f9be93e6b91e2d39a175717361cc900b78b4a1e
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-10-29 14:13:10 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "360px",
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+        "2xl": "1440px",
+      },
+      colors: {
+        white: "#EFFFFB",
+        green: "#50D890",
+        black: "#272727",
+      },
+    },
+  },
+  plugins: [],
+};
