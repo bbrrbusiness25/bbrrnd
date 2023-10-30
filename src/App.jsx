2705dd23c91484ef276da8a6d2516d299e491b56
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-10-30 21:32:39 +0530

    done with features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Articles from "./pages/Articles";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <main>
       <Navbar />
       <Articles />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t border-gray-300 py-6">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-center items-center">
+          <p className="text-black text-sm">
+            Made with ❤️ by{" "}
+            <a
+              href="https://www.linkedin.com/in/shaikahmadnawaz"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-green-500 font-bold hover:underline"
+            >
+              Shaik Ahmad Nawaz
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
